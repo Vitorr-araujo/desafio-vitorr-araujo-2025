b183c0a6e1fb8cb19e9b9699041e5cd7614561c9
VItor Carvalho de Araujo <105469574+vitorr-araujo@users.noreply.github.com>
2025-09-06 21:36:58 -0300

    atualização teste

---
+++ src/abrigo-animais.js
@@ -0,0 +1,50 @@
+class AbrigoAnimais {
+  constructor() {
+    this.animais = [
+      {
+      nome: 'Rex',
+      especie: 'cão',
+      brinquedos: ['RATO', 'BOLA']
+      }
+      {
+      nome: 'Mimi',
+      especie: 'gato',
+      brinquedos: ['BOLA','LASER']
+      }
+      {
+      nome: 'Fofo',
+      especie: 'gato',
+      brinquedos: ['BOLA','RATO','LASER']
+      }
+      {
+      nome: 'Zero',
+      especie: 'gato',
+      brinquedos: ['RATO','BOLA']
+      }
+      {
+      nome: 'Bola',
+      especie: 'cão',
+      brinquedos: ['CAIXA','NOVELO']
+      }
+      {
+      nome: 'Bebe',
+      especie: 'cão',
+      brinquedos: ['LASER','RATO','BOLA']
+      }
+      {
+      nome: 'Loco',
+      especie: 'jabuti',
+      brinquedos: ['SKATE','RATO']
+      }
+    ]
+  }
+
+
+  encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
+    const lista
+  
+  
+  }
+}
+
+export { AbrigoAnimais as AbrigoAnimais };
